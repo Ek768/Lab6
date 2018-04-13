ac3a3d2622f094736cec0c0b0cc9a0ada197d4fb
Ekaterina <bazhina.ekaterina18@yandex.ru>
2018-04-13 22:36:11 +0300

    Первоначальный коммит

---
+++ ConsoleApplication2/Gitara.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Gitara: Tovar
+    {
+        public string Vid_instrumenta { get; set; }
+        public string Marka_instrumenta { get; set; }
+        public DateTime Release_data { get; set; }
+
+        public Gitara(Nomenklatura ID, double Price, string Vid_instrumenta, string Marka_instrumenta, DateTime Release_data) : base(ID, Price)
+        {
+            this.Vid_instrumenta = Vid_instrumenta;
+            this.Marka_instrumenta = Marka_instrumenta;
+            this.Release_data = Release_data;
+        }
+        public override void Get_info()
+        {
+            Console.WriteLine(ID.ToString() + ", " + Price.ToString() + ", " + Vid_instrumenta.ToString() + ", " + Marka_instrumenta.ToString() + ", " + Release_data.ToString("dd MMMM, yyyy"));
+        }
+    }
+}
---
+++ ConsoleApplication2/Tovar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Tovar
+    {
+        public Nomenklatura ID { get; set; }
+        public double Price { get; set; }
+
+        public Tovar(Nomenklatura ID, double Price)
+        {
+            this.ID = ID;
+            this.Price = Price;
+        }
+
+        public virtual void Get_info()
+        {
+            Console.WriteLine(ID.ToString() + ", " + Price.ToString());
+        }
+    }
+}
